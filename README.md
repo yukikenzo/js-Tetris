88ee7f1a19510589c0aeef5e7080aaa2ebd699a4
yukikenzo <gentlejp1122@gmail.com>
2024-03-13 04:26:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Tetris Game
+
+This project is a simple yet fully functional implementation of the classic Tetris game using pure **HTML**, **CSS**, and **JavaScript**, with no external libraries. It's designed to be lightweight and straightforward, suitable for beginners looking to understand game development with web technologies.
+
+## How to Play
+
+To play the game, follow these steps:
+
+1. Clone the repository or download the ZIP file.
+2. If downloaded as a ZIP, unzip the file.
+3. Navigate to the project directory.
+4. Double-click on `play.html` to open the game in your web browser.
+
+### Game Controls
+
+- **_Enter_**: Start the game
+- **_Arrow Left_**: Move left
+- **_Arrow Right_**: Move right
+- **_Arrow Up_**: Rotate the piece
+- **_Arrow Down_**: Drop the piece faster
+- **_Space_**: Pause/Resume the game
+
+Enjoy the classic block-stacking game where the goal is to clear as many lines as possible by completing horizontal rows of blocks without any gaps.
+
+## Features
+
+- Start, pause, and resume gameplay
+- Keyboard controls for moving and rotating Tetris blocks
+- Increasing difficulty as you progress in the game
+
+## Contributing
+
+Your contributions are welcome! If you'd like to improve the Tetris game, please check out the [contributing guidelines](https://github.com/vontanne/javascript-tetris/blob/main/.github/CONTRIBUTING.md).
+
+## License
+
+This game is open-sourced under the [MIT license](https://github.com/vontanne/javascript-tetris/blob/main/LICENSE).
---
+++ src/js/script.js
@@ -0,0 +1,546 @@
+class Game {
+  static points = {
+    1: 10,
+    2: 30,
+    3: 90,
+    4: 270,
+  };
+
+  constructor() {
+    this.reset();
+  }
+
+  get level() {
+    return Math.floor(this.lines * 0.04);
+  }
+
+  getState() {
+    const playfield = this.createPlayfield();
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < this.playfield.length; y++) {
+      playfield[y] = [];
+
+      for (let x = 0; x < this.playfield[y].length; x++) {
+        playfield[y][x] = this.playfield[y][x];
+      }
+    }
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+
+    return {
+      score: this.score,
+      level: this.level,
+      lines: this.lines,
+      nextPiece: this.nextPiece,
+      playfield,
+      isGameOver: this.topOut,
+    };
+  }
+
+  reset() {
+    this.score = 0;
+    this.lines = 0;
+    this.topOut = false;
+    this.playfield = this.createPlayfield();
+    this.activePiece = this.createPiece();
+    this.nextPiece = this.createPiece();
+  }
+
+  createPlayfield() {
+    const playfield = [];
+
+    for (let y = 0; y < 20; y++) {
+      playfield[y] = [];
+      for (let x = 0; x < 10; x++) {
+        playfield[y][x] = 0;
+      }
+    }
+
+    return playfield;
+  }
+
+  createPiece() {
+    const index = Math.floor(Math.random() * 7);
+    const type = "IJLQSTZ"[index];
+    const piece = {};
+
+    switch (type) {
+      case "I":
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [1, 1, 1, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+        ];
+        break;
+      case "J":
+        piece.blocks = [
+          [0, 0, 0],
+          [2, 2, 2],
+          [0, 0, 2],
+        ];
+        break;
+      case "L":
+        piece.blocks = [
+          [0, 0, 0],
+          [3, 3, 3],
+          [3, 0, 0],
+        ];
+        break;
+      case "Q":
+        piece.blocks = [
+          [0, 0, 0, 0],
+          [0, 4, 4, 0],
+          [0, 4, 4, 0],
+          [0, 0, 0, 0],
+        ];
+        break;
+      case "S":
+        piece.blocks = [
+          [0, 0, 0],
+          [0, 5, 5],
+          [5, 5, 0],
+        ];
+        break;
+      case "T":
+        piece.blocks = [
+          [0, 0, 0],
+          [6, 6, 6],
+          [0, 6, 0],
+        ];
+        break;
+      case "Z":
+        piece.blocks = [
+          [0, 0, 0],
+          [7, 7, 0],
+          [0, 7, 7],
+        ];
+        break;
+      default:
+        throw new Error("Oops!");
+    }
+
+    piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+    piece.y = -1;
+
+    return piece;
+  }
+
+  movePieceLeft() {
+    this.activePiece.x -= 1;
+    if (this.hasCollision()) {
+      this.activePiece.x += 1;
+    }
+  }
+
+  movePieceRigth() {
+    this.activePiece.x += 1;
+    if (this.hasCollision()) {
+      this.activePiece.x -= 1;
+    }
+  }
+
+  movePieceDown() {
+    if (this.topOut) return;
+
+    this.activePiece.y += 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.y -= 1;
+      this.lockPiece();
+      const clearedLines = this.clearLines();
+      this.updateScore(clearedLines);
+      this.updatePieces();
+    }
+
+    if (this.hasCollision()) {
+      this.topOut = true;
+    }
+  }
+
+  rotatePiece() {
+    this.rotateBlocks();
+
+    if (this.hasCollision()) {
+      this.rotateBlocks(false);
+    }
+  }
+
+  rotateBlocks(clockwise = true) {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    const x = Math.floor(length / 2);
+    const y = length - 1;
+
+    for (let i = 0; i < x; i++) {
+      for (let j = i; j < y - i; j++) {
+        const temp = blocks[i][j];
+
+        if (clockwise) {
+          blocks[i][j] = blocks[y - j][i];
+          blocks[y - j][i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[j][y - i];
+          blocks[j][y - i] = temp;
+        } else {
+          blocks[i][j] = blocks[j][y - i];
+          blocks[j][y - i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[y - j][i];
+          blocks[y - j][i] = temp;
+        }
+      }
+    }
+  }
+
+  hasCollision() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (
+          blocks[y][x] &&
+          (this.playfield[pieceY + y] === undefined ||
+            this.playfield[pieceY + y][pieceX + x] === undefined ||
+            this.playfield[pieceY + y][pieceX + x])
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  lockPiece() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
+
+  clearLines() {
+    const rows = 20;
+    const columns = 10;
+    let lines = [];
+
+    for (let y = rows - 1; y >= 0; y--) {
+      let numberOfBlocks = 0;
+
+      for (let x = 0; x < columns; x++) {
+        if (this.playfield[y][x]) {
+          numberOfBlocks += 1;
+        }
+      }
+
+      if (numberOfBlocks === 0) {
+        break;
+      } else if (numberOfBlocks < columns) {
+        continue;
+      } else if (numberOfBlocks === columns) {
+        lines.unshift(y);
+      }
+    }
+
+    for (let index of lines) {
+      this.playfield.splice(index, 1);
+      this.playfield.unshift(new Array(columns).fill(0));
+    }
+
+    return lines.length;
+  }
+
+  updateScore(clearedLines) {
+    if (clearedLines > 0) {
+      this.score += Game.points[clearedLines] * (this.level + 1);
+      this.lines += clearedLines;
+    }
+  }
+
+  updatePieces() {
+    this.activePiece = this.nextPiece;
+    this.nextPiece = this.createPiece();
+  }
+}
+
+class View {
+  static colors = {
+    1: "#0000ff",
+    2: "#00ff00",
+    3: "#ff0000",
+    4: "#ff8000",
+    5: "#ff00ff",
+    6: "#ffff00",
+    7: "#00ffff",
+  };
+
+  constructor(element, width, height, rows, columns) {
+    this.element = element;
+    this.width = width;
+    this.height = height;
+
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.context = this.canvas.getContext("2d");
+
+    this.playfieldBorderWidth = 1;
+    this.playfieldX = this.playfieldBorderWidth;
+    this.playfieldY = this.playfieldBorderWidth;
+    this.playfieldWidth = (this.width * 2) / 3;
+    this.playfieldHeight = this.height;
+    this.playfieldInnerWidth =
+      this.playfieldWidth - this.playfieldBorderWidth * 2;
+    this.playfieldInnerHeight =
+      this.playfieldHeight - this.playfieldBorderWidth * 2;
+
+    this.blockWidth = this.playfieldInnerWidth / columns;
+    this.blockHeight = this.playfieldInnerHeight / rows;
+
+    this.panelX = this.playfieldWidth + 10;
+    this.panelY = 0;
+    this.panelWidth = this.width / 3;
+    this.panelHeight = this.height;
+
+    this.element.appendChild(this.canvas);
+  }
+
+  renderMainScreen(state) {
+    this.clearScreen();
+    this.renderPlayfield(state);
+    this.renderPanel(state);
+  }
+
+  renderStartScreen() {
+    this.context.fillStyle = "#fff";
+    this.context.font = "18px 'Courier New'";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillText(
+      "Press Enter to Start",
+      this.width / 3,
+      this.height / 2
+    );
+  }
+
+  renderPauseScreen() {
+    this.context.fillStyle = "rgba(0, 0, 0, 0.8)";
+    this.context.fillRect(0, 0, this.width, this.height);
+
+    this.context.fillStyle = "#fff";
+    this.context.font = "18px 'Courier New'";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillText(
+      "Press Enter to Resume",
+      this.width / 3,
+      this.height / 2
+    );
+  }
+
+  renderEndScreen({ score }) {
+    this.clearScreen();
+
+    this.context.fillStyle = "#fff";
+    this.context.font = "18px 'Courier New'";
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillText("GAME OVER", this.width / 3, this.height / 2 - 48);
+    this.context.fillText(`Score: ${score}`, this.width / 3, this.height / 2);
+    this.context.fillText(
+      "Press Enter to Restart",
+      this.width / 3,
+      this.height / 2 + 48
+    );
+  }
+
+  clearScreen() {
+    this.context.clearRect(0, 0, this.width, this.height);
+  }
+
+  renderPlayfield({ playfield }) {
+    for (let y = 0; y < playfield.length; y++) {
+      for (let x = 0; x < playfield[y].length; x++) {
+        const block = playfield[y][x];
+
+        if (block) {
+          this.renderBlock(
+            this.playfieldX + x * this.blockWidth,
+            this.playfieldY + y * this.blockHeight,
+            this.blockWidth,
+            this.blockHeight,
+            View.colors[block]
+          );
+        }
+      }
+    }
+
+    this.context.strokeStyle = "#fff";
+    this.context.lineWidth = this.playfieldBorderWidth;
+    this.context.strokeRect(0, 0, this.playfieldWidth, this.playfieldHeight);
+  }
+
+  renderPanel({ level, score, lines, nextPiece }) {
+    this.context.textAlign = "start";
+    this.context.textBaseline = "top";
+    this.context.fillStyle = "#fff";
+    this.context.font = "14px 'Courier New'";
+
+    this.context.fillText(`Score: ${score}`, this.panelX, this.panelY + 0);
+    this.context.fillText(`Lines: ${lines}`, this.panelX, this.panelY + 24);
+    this.context.fillText(`Level: ${level}`, this.panelX, this.panelY + 48);
+    this.context.fillText("Next:", this.panelX, this.panelY + 96);
+
+    for (let y = 0; y < nextPiece.blocks.length; y++) {
+      for (let x = 0; x < nextPiece.blocks[y].length; x++) {
+        const block = nextPiece.blocks[y][x];
+
+        if (block) {
+          this.renderBlock(
+            this.panelX + x * this.blockWidth * 0.5,
+            this.panelY + 100 + y * this.blockHeight * 0.5,
+            this.blockWidth * 0.5,
+            this.blockHeight * 0.5,
+            View.colors[block]
+          );
+        }
+      }
+    }
+  }
+
+  renderBlock(x, y, width, height, color) {
+    this.context.fillStyle = color;
+    this.context.strokeStyle = "#000";
+    this.context.lineWidth = 2;
+
+    this.context.fillRect(x, y, width, height);
+    this.context.strokeRect(x, y, width, height);
+  }
+}
+
+class Controller {
+  constructor(game, view) {
+    this.game = game;
+    this.view = view;
+    this.intervalId = null;
+    this.isPlaying = false;
+
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+    document.addEventListener("keyup", this.handleKeyUp.bind(this));
+    this.view.renderStartScreen();
+  }
+
+  update() {
+    this.game.movePieceDown();
+    this.updateView();
+  }
+
+  play() {
+    this.isPlaying = true;
+    this.startTimer();
+    this.updateView;
+  }
+
+  pause() {
+    this.isPlaying = false;
+    this.stopTimer();
+    this.updateView();
+  }
+
+  reset() {
+    this.game.reset();
+    this.play();
+  }
+
+  updateView() {
+    const state = this.game.getState();
+
+    if (state.isGameOver) {
+      this.view.renderEndScreen(state);
+    } else if (!this.isPlaying) {
+      this.view.renderPauseScreen();
+    } else {
+      this.view.renderMainScreen(state);
+    }
+  }
+
+  startTimer() {
+    const speed = 1000 - this.game.getState().level * 100;
+    if (!this.intervalId) {
+      this.intervalId = setInterval(
+        () => {
+          this.update();
+        },
+        speed > 0 ? speed : 100
+      );
+    }
+  }
+
+  stopTimer() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+
+  handleKeyDown(event) {
+    const state = this.game.getState();
+    switch (event.keyCode) {
+      case 32:
+        if (state.isGameOver) {
+          this.reset();
+        } else if (this.isPlaying) {
+          this.pause();
+        } else {
+          this.play();
+        }
+        break;
+      case 13:
+        if (state.isGameOver) {
+          this.reset();
+        } else if (this.isPlaying) {
+          this.pause();
+        } else {
+          this.play();
+        }
+        break;
+      case 37:
+        this.game.movePieceLeft();
+        this.updateView();
+        break;
+      case 38:
+        this.game.rotatePiece();
+        this.updateView();
+        break;
+      case 39:
+        this.game.movePieceRigth();
+        this.updateView();
+        break;
+      case 40:
+        this.stopTimer();
+        this.game.movePieceDown();
+        this.updateView();
+        break;
+    }
+  }
+
+  handleKeyUp(event) {
+    switch (event.keyCode) {
+      case 40:
+        this.startTimer();
+        break;
+    }
+  }
+}
+
+const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
+const controller = new Controller(game, view);
